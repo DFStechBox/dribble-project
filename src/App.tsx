5e5016fbf930cc48444333691f26183bf11ce6de
dfosterstewart <dfosterstewart@gmail.com>
2024-01-19 10:26:13 -0800

    Setup PricingCard & App components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-
-import './App.css'
+import PricingCard from './assets/components/PricingCard/PricingCard';
+import './App.css';
 
 function App() {
   
@@ -7,7 +7,7 @@ function App() {
   return (
   
       <div>
-        
+        <PricingCard />
       </div>
      
   
